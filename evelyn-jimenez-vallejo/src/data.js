b62d04a621a5eba87ccf843ebedce277e1d6aa50
evejiva12-hue <evejiva12@gmail.com>
2026-03-28 11:59:37 -0500

    se agrego los datos de los destinos en el archivo data

---
+++ evelyn-jimenez-vallejo/src/data.js
@@ -0,0 +1,36 @@
+// src/data.js
+
+export const destinosTuristicos = [
+  {
+    id: 1,
+    pais: "Japón",
+    ciudad: "Tokio",
+    duracionDias: 10,
+    precioCOP: 11250000,
+    imagenDinamica: "https://images.unsplash.com/photo-1493976040374-85c8e12f0c0e?auto=format&fit=crop&w=500"
+  },
+  {
+    id: 2,
+    pais: "Francia",
+    ciudad: "París",
+    duracionDias: 7,
+    precioCOP: 8100000,
+    imagenDinamica: "https://images.unsplash.com/photo-1502602898657-3e91760cbb34?auto=format&fit=crop&w=500"
+  },
+  {
+    id: 3,
+    pais: "Italia",
+    ciudad: "Roma",
+    duracionDias: 8,
+    precioCOP: 9450000,
+    imagenDinamica: "https://images.unsplash.com/photo-1552832230-c0197dd311b5?auto=format&fit=crop&w=500"
+  },
+  {
+    id: 4,
+    pais: "Egipto",
+    ciudad: "El Cairo",
+    duracionDias: 12,
+    precioCOP: 6750000,
+    imagenDinamica: "https://images.unsplash.com/photo-1503177119275-0aa32b3a7447?auto=format&fit=crop&w=500"
+  }
+];
