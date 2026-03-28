e31ac51faf33705e0c1900b9544aebd5fd94ff24
GodBlessedHans <haseveco@gmail.com>
2026-03-28 12:18:54 -0500

    Creacion de datos locales en (data.js)

---
+++ hans-velasquez/src/data.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export const destinos=[
+    {id:1, pais:"Mexico",ciudad: "Cancún", precio:5000 , duracionDias: 7, imagen:"https://www.cancun.travel/wp-content/uploads/2019/11/cancun-beach.jpg"},
+    {id:2, pais:"Venezuela", ciudad: "Isla Mujeres", precio:4500, duracionDias: 5, imagen:"https://www.aruba.com/wp-content/uploads/2020/03/aruba-beach.jpg"},
+    {id:3, pais:"Brasil", ciudad: "Rio de Janeiro", precio:4000, duracionDias: 8, imagen:"https://www.brazil.travel/wp-content/uploads/2020/03/brazil-beach.jpg"},
+    {id:4, pais:"Japón", ciudad: "Tokyo", precio:3500, duracionDias: 10, imagen:"https://www.japan.travel/wp-content/uploads/2020/03/japan-tourism.jpg"},
+    {id:5, pais:"Jamaica", ciudad: "Kingston", precio:3000, duracionDias: 6, imagen:"https://www.jamaica.com/wp-content/uploads/2020/03/jamaica-beach.jpg"}]
+
+export default { destinos }
