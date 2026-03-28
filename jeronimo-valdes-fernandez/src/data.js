1247d40d2bc21f37761a547b71daf0eaaaa39267
desarrollo <desarrollo@academicos.cesde.edu.co>
2026-03-28 12:24:59 -0500

    iniciando proyecto vite

---
+++ jeronimo-valdes-fernandez/src/data.js
@@ -0,0 +1,35 @@
+export const cursos = [
+  {
+    id: 1,
+    nombreCurso: "Front 2",
+    profesor: "Jaime Zapata",
+    horasSemanales: 10,
+    nivel: "Avanzado",
+    imagen: "https://www.shutterstock.com/image-photo/professor-holding-book-pointing-wooden-260nw-1114011842.jpg"
+  },
+  {
+    id: 2,
+    nombreCurso: "Back 1",
+    profesor: "Samuel Cano",
+    horasSemanales: 8,
+    nivel: "Basico",
+    imagen: "https://previews.123rf.com/images/ostill/ostill1112/ostill111200417/11752832-man-caucasian-teacher-professor-lecturing-isolated-studio-on-white-background.jpg"
+
+  },
+  {
+    id: 3,
+    nombreCurso: "Back 2",
+    profesor: "Fabio Dominguez",
+    horasSemanales: 14,
+    nivel: "Avanzado",
+    imagen: "https://img.freepik.com/foto-gratis/retrato-joven-feliz-gafas_171337-4850.jpg?semt=ais_incoming&w=740&q=80"
+  },
+  {
+    id: 4,
+    nombreCurso: "Front 1",
+    profesor: "Jaime Camilo Murillo",
+    horasSemanales: 8,
+    nivel: "Intermedio",
+    imagen: "https://media.istockphoto.com/id/1226086641/es/foto/joven-profesor-con-blazer-azul-camisa-casual-y-pantalones-sosteniendo-los-brazos-cruzados.jpg?s=612x612&w=0&k=20&c=AKstHN1069sms9dDcnBPlKdpWYZ1M97tBq8Q7S5Yrg4="
+  }
+  ];
