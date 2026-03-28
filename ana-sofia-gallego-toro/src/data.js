710c2d0f76574914f6a7ab6a61838bb48687aa1b
anasofiag09 <anasofiagallego.908@gmail.com>
2026-03-28 12:14:33 -0500

    creacion de data con peliculas quemadas

---
+++ ana-sofia-gallego-toro/src/data.js
@@ -0,0 +1,34 @@
+export const peliculas = [
+  {
+    id: 1,
+    titulo: "My Little Pony: la película",
+    director: "Jayson Thiessen",
+    genero: "drama",
+    duracion: "99 minutos",
+    poster: "https://static.wikia.nocookie.net/mlp/images/7/7d/My_Little_Pony_La_Pel%C3%ADcula_p%C3%B3ster_oficial_latino.jpg/revision/latest/scale-to-width-down/1200?cb=20170716060714&path-prefix=es"
+  },
+  {
+    id: 2,
+    titulo: "Titanic",
+    director: "James Cameron",
+    genero: "Romance",
+    duracion: "195 minutos",
+    poster: "https://play-lh.googleusercontent.com/Vb1dfuu9ga-SwD_dWHVVYsia93uElzfKvLlSSTdGFCpNJ8VIuTpqtv_SXQ56qpX0gxTS"
+  },
+  {
+    id: 3,
+    titulo: "Avengers infinity war",
+    director: "Marvel Studios",
+    genero: "Acción",
+    duracion: "143 minutos",
+    poster: "https://static.wikia.nocookie.net/marvelcinematicuniverse/images/9/94/Avengers_Infinity_War_-_P%C3%B3ster_oficial.png/revision/latest/scale-to-width-down/1200?cb=20191029195530&path-prefix=es"
+  },
+  {
+    id: 4,
+    titulo: "Monster High: Amor Monstruoso",
+    director: "Steve Sacks y Dustin McKenzie",
+    genero: "drama",
+    duracion: "60 minutos",
+    poster: "https://static.wikia.nocookie.net/wiki-doblaje-espana/images/d/d4/Monster_High_Romance_monstruoso.png/revision/latest?cb=20240820221917&path-prefix=es"
+  }
+];
