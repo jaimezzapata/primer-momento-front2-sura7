34cd27d974679dc07bd4cb38c6212649d9c0b169
felipesg44 <sierragomezandres@gmail.com>
2026-03-28 11:48:22 -0500

    feat: se agrega archivo data.js con la informacion mockeada de los cursos

---
+++ andres-felipe-sierra-gomez/src/data.js
@@ -0,0 +1,34 @@
+export const coursesData = [
+  {
+    id: 1,
+    nombreCurso: "Introduccion a la informatica",
+    profesor: "Carlos Mendoza",
+    horasSemanales: 10,
+    nivel: "Basico",
+    imagen: "https://blogdeinformaticadario.wordpress.com/wp-content/uploads/2015/11/introduccion.png"
+  },
+  {
+    id: 2,
+    nombreCurso: "Backend I",
+    profesor: "Laura Gómez",
+    horasSemanales: 6,
+    nivel: "Básico",
+    imagen: "https://media.licdn.com/dms/image/v2/D4D12AQFnxb2cyB4ExQ/article-cover_image-shrink_600_2000/article-cover_image-shrink_600_2000/0/1669297731190?e=2147483647&v=beta&t=sRnBowDzBe2IKcdXjmCd8R94qJ4SlAJgst-0QiUIEEA"
+  },
+  {
+    id: 3,
+    nombreCurso: "Frontend I",
+    profesor: "Andrés Vargas",
+    horasSemanales: 8,
+    nivel: "Básico",
+    imagen: "https://media.licdn.com/dms/image/v2/D4D12AQE9KDPe-KE_cQ/article-cover_image-shrink_600_2000/article-cover_image-shrink_600_2000/0/1695744816253?e=2147483647&v=beta&t=RguMpFA-kyn-SWUnlVIynwWYC0OrNUzh2zL2VM4J_3g"
+  },
+  {
+    id: 4,
+    nombreCurso: "Fundamentos de Bases de Datos y SQL",
+    profesor: "Diana Rojas",
+    horasSemanales: 12,
+    nivel: "Avanzado",
+    imagen: "https://media.licdn.com/dms/image/v2/C4E12AQHWe6Iy5JPBjw/article-cover_image-shrink_600_2000/article-cover_image-shrink_600_2000/0/1618928046769?e=2147483647&v=beta&t=Hu9mzxZ-DOAGaZ_O_6Bte03F6MQS4X95G0Ie29SCZqA"
+  }
+];
