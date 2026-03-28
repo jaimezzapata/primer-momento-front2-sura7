3d084df4cecf91f6f0c00f52bfe2e46f56a98341
evejiva12-hue <evejiva12@gmail.com>
2026-03-28 12:40:42 -0500

    se importan los datos y los estilos de las cards

--- evelyn-jimenez-vallejo/src/App.jsx
+++ evelyn-jimenez-vallejo/src/App.jsx
@@ -1,1 +1,22 @@
+import { destinosTuristicos } from './data';
+import DestinosCard from './DestinosCard';
 
+function App() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Agencia Mundo Sin Fronteras</h1>
+      <div style={{ 
+        display: 'grid', 
+        gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))', 
+        gap: '20px' 
+      }}>
+        {/* Aquí usas .map() para cumplir con el criterio 6 de la rúbrica */}
+        {destinosTuristicos.map((destino) => (
+          <DestinosCard key={destino.id} viaje={destino} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
