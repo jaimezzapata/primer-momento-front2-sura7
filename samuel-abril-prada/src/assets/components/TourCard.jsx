1065d91f150b8383d7ab26925deed2fa36dc2874
Samuel abril <sabrilprr@gmail.com>
2026-03-28 22:36:51 -0500

    se definen estilos para las cartas imagenes y se implementan en TourCard el componente

--- samuel-abril-prada/src/assets/components/TourCard.jsx
+++ samuel-abril-prada/src/assets/components/TourCard.jsx
@@ -1,15 +1,14 @@
-
 const TourCard = ({ info }) => {
     return (
-        <div>
+        <div className="tour-card">
             <img src={info.imagen} alt={"Concierto en ${info.ciudad}"} />
             
-            <div>
+            <div className="tour-card-content">
                 <p><strong>ID:</strong> {info.id}</p>
                 <h2>{info.ciudad}</h2>
                 <p><strong>Fecha:</strong> {info.fecha}</p>
                 <p><strong>Recinto:</strong> {info.recinto}</p>
-                <p><strong>Estado:</strong> {info.estadoBoletas}</p>
+                <p className="status"><strong>Estado:</strong> {info.estadoBoletas}</p>
             </div>
         </div>
     );
