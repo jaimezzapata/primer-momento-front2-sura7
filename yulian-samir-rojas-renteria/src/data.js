be2e9ff4abe1431e9e1512c3e2f8c3ef6c39d085
yulianrojas886@gmail.com <cuentasgeorge13@gmail.com>
2026-03-30 11:50:58 -0500

    Se añaden imagenes a las tarjetas

--- yulian-samir-rojas-renteria/src/data.js
+++ yulian-samir-rojas-renteria/src/data.js
@@ -5,7 +5,7 @@ export const destinos = [
     ciudad: "Tokio",
     duracionDias: 10,
     precioDolares: 2500,
-    imagenDinamica: "https://unsplash.com/es/s/fotos/ciudad-de-tokio"
+    imagenDinamica: "https://plus.unsplash.com/premium_photo-1661914240950-b0124f20a5c1?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y2l1ZGFkJTIwZGUlMjB0b2tpb3xlbnwwfHwwfHx8MA%3D%3D"
   },
   {
     id: 2,
@@ -13,7 +13,7 @@ export const destinos = [
     ciudad: "París",
     duracionDias: 7,
     precioDolares: 1800,
-    imagenDinamica: "https://images.unsplash.com"
+    imagenDinamica: "https://images.unsplash.com/photo-1502602898657-3e91760cbb34?auto=format&fit=crop&w=1500&q=80"
   },
   {
     id: 3,
@@ -21,7 +21,7 @@ export const destinos = [
     ciudad: "Roma",
     duracionDias: 8,
     precioDolares: 2100,
-    imagenDinamica: "https://images.unsplash.com"
+    imagenDinamica: "https://www.turismoroma.it/sites/default/files/Roma%20in%20breve.jpg"
   },
   {
     id: 4,
@@ -29,6 +29,6 @@ export const destinos = [
     ciudad: "El Cairo",
     duracionDias: 12,
     precioDolares: 3000,
-    imagenDinamica: "https://images.unsplash.com"
+    imagenDinamica: "https://www.civitatis.com/blog/wp-content/uploads/2025/01/que-ver-cairo-egipto.jpg"
   }
 ];
