44baf98d361bd24c6ef73ec8a8b759ec090e57c2
kr1pt0n28 <cuentasgeorge13@gmail.com>
2026-03-30 08:59:12 -0500

    creacion de estructura App.jsx

--- jorge-florez/src/data.js
+++ jorge-florez/src/data.js
@@ -1,8 +1,36 @@
 const productos = [
-  { id: 1, nombreProducto: 'Smartphone Pro', precio: 2399000, categoria: 'Electrónica', imagen: '' },
-  { id: 2, nombreProducto: 'Auriculares Bluetooth', precio: 259900, categoria: 'Accesorios', imagen: '' },
-  { id: 3, nombreProducto: 'Teclado mecánico', precio: 189900, categoria: 'Periféricos', imagen: '' },
-  { id: 4, nombreProducto: 'Cámara web HD', precio: 179900, categoria: 'Video', imagen: '' }
+  {
+    id: 1,
+    nombreProducto: 'Smartphone Pro',
+    precio: 2399000,
+    categoria: 'Smartphones',
+    imagen:
+      ''
+  },
+  {
+    id: 2,
+    nombreProducto: 'Auriculares Bluetooth',
+    precio: 259900,
+    categoria: 'Audio',
+    imagen:
+      ''
+  },
+  {
+    id: 3,
+    nombreProducto: 'Teclado mecánico',
+    precio: 189900,
+    categoria: 'Periféricos',
+    imagen:
+      ''
+  },
+  {
+    id: 4,
+    nombreProducto: 'Cámara web HD',
+    precio: 179900,
+    categoria: 'Video',
+    imagen:
+      'h'
+  }
 ];
 
 export default productos;
