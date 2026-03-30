dd9a4b561841980e8fc2a39026e46c810d05e47a
kr1pt0n28 <cuentasgeorge13@gmail.com>
2026-03-30 08:46:21 -0500

    se agrega data.js y techcard en carpeta components

---
+++ jorge-florez/src/data.js
@@ -0,0 +1,9 @@
+const productos = [
+  { id: 1, nombreProducto: 'Smartphone Pro', precio: 2399000, categoria: 'Electrónica', imagen: '' },
+  { id: 2, nombreProducto: 'Auriculares Bluetooth', precio: 259900, categoria: 'Accesorios', imagen: '' },
+  { id: 3, nombreProducto: 'Teclado mecánico', precio: 189900, categoria: 'Periféricos', imagen: '' },
+  { id: 4, nombreProducto: 'Cámara web HD', precio: 179900, categoria: 'Video', imagen: '' }
+];
+
+export default productos;
+
